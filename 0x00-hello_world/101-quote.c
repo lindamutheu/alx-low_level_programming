a177817694ddb93171173e61f88a398f93224304
lindamutheu <lindamutheu5@gmail.com>
2022-04-09 04:13:13 -0700

    My quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include<unistd.h>
+#include <unistd.h>
+
 /**
- *main - Function begins
- *
- *Return:Function ends
- */
+  * main - Prints a text
+  *
+  * Return: Always 1 (Success)
+  */
 int main(void)
 {
-  write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-  return (1)
-    }
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}

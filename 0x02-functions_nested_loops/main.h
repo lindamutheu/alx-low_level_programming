bcbc3b3d379c97f28463f25136106428bf41e997
lindamutheu <lindamutheu5@gmail.com>
2022-04-12 07:33:56 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+Void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+Void jack_bauer(void);
+Void times_tables(void);
+int add(int,int);
+Void print_to_98(int n);
+Void print_times_table(int n);
+
+#endif
+
+
